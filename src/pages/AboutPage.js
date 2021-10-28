f9a315d62aee74a3b3f0102f5a2bd3359b8c49dc
Ayush <ayush2089@gmail.com>
2021-10-28 15:03:49 +0530

    :sparkles: About Page Created.

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,43 @@
+import "../assets/css/pages/AboutPage.css";
+import { useMediaQuery } from "react-responsive";
+import { useEffect, useState } from "react";
+import AboutSection from "../components/AboutSection";
+import NavbarDesktop from "../components/NavbarDesktop";
+import NavbarMobile from "../components/NavbarMobile";
+import SocialIcons from "../components/SocialIcons";
+
+const About = () => {
+  const isMobile = useMediaQuery({ query: `(max-width: 640px)` });
+  const [hamMenu, setHamMenu] = useState(0);
+
+  useEffect(() => {
+    if (!isMobile) setHamMenu(0);
+  }, [isMobile]);
+
+  return (
+    <div className="w-100">
+      {/* ******************** */}
+      {/* Navbar               */}
+      {/* ******************** */}
+      {isMobile ? (
+        <NavbarMobile
+          textColor="black"
+          hamMenu={hamMenu}
+          setHamMenu={setHamMenu}
+        />
+      ) : (
+        <NavbarDesktop textColor="black" />
+      )}
+      {/* ******************** */}
+      {/* About Content        */}
+      {/* ******************** */}
+      {!hamMenu && <AboutSection />}
+      {/* ******************** */}
+      {/* Social Icon          */}
+      {/* ******************** */}
+      {hamMenu ? <SocialIcons /> : <></>}
+    </div>
+  );
+};
+
+export default About;
